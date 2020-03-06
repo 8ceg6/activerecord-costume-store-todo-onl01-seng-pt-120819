2b798402b4b4797ac9a7ed41f8f66253989adfbb
cedric e goddard <8ceg6@comcast.net>
2020-03-06 00:45:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,5 +4,11 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   
   def change 
       create_table :haunted_houses
+      string :name 
+      string :location 
+      string :theme 
+      integer :price
+      boolean :family_friendly
+      date :opening_date
       
   end 
